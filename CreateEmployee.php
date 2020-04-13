02e77878f02960980dca51347f577dd5f393293d
Joe Pauker <51676427+joePauker1@users.noreply.github.com>
2020-04-12 21:53:25 -0400

    Create CreateEmployee.php

---
+++ CreateEmployee.php
@@ -0,0 +1,32 @@
+<?php
+include "config.php";
+
+$con = new mysqli($server, $login, $password, $dbname) or die("<br>Cannot connect to Database");
+
+$fName = $_POST['fName'];
+$lName = $_POST['lName'];
+$email = $_POST['email'];
+$pswd = $_POST['password'];
+$phoneNum = $_POST['phoneNum'];
+
+    $sql_query = "SELECT count(*) as emailCT FROM User_Employee WHERE '$email' = Email";
+    $result = mysqli_query($con, $sql_query);
+    $row = mysqli_fetch_array($result);
+    if($result) {
+        $count = $row['emailCT'];
+        if ($fName != " " && $lName != " " && $email != " " && $pswd != " " && $phoneNum != " ") {
+            if($count <= 0){
+                $insert = "INSERT INTO User_Employee(FName, LName, Email, Password, PhoneNum)
+                            VALUES ('$fName', '$lName', '$email', SHA('$pswd'), '$phoneNum')";
+                $con->query($insert);
+                echo "Successfully created a account!";
+            }      
+            else {
+                echo "Email already has account!";
+            }
+        }
+    }
+    else {
+    echo "There was an issue with account being created!";
+    }
+?>
