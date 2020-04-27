95a7f83492741cd3135cad39b6042b3f89c67857
Joe Pauker <51676427+joePauker1@users.noreply.github.com>
2020-04-27 17:03:50 -0400

    Update User_loginEmpr.php

--- newfiles/User_loginEmpr.php
+++ newfiles/User_loginEmpr.php
@@ -1,24 +1,32 @@
 <?php
+session_start();
 include "config.php";
 
 $con = new mysqli($server, $login, $password, $dbname) or die("<br>Cannot connect to Database");
 
-$cookie_n = "name";
+$cookie_n = "person";
 $uname = $_POST['username'];
 $password = $_POST['password'];
 
 
 if ($uname != "" && $password != "") {
 
-    $sql_EMPR = "SELECT EmprID, count(*) AS cntUsers, Password as passEMPR FROM User_Employer WHERE Email='" . $uname . "'";
+    $sql_EMPR = "SELECT CompanyName, Email, PhoneNum, EmprID, count(*) AS cntUsers, Password as passEMPR FROM User_Employer WHERE Email='" . $uname . "'";
     $EMPRresult = mysqli_query($con, $sql_EMPR);
     $row = mysqli_fetch_array($EMPRresult);
     $countEMPR = $row['cntUsers'];
     $passEMPR = $row['passEMPR'];
-
+    $CompanyName = $row['CompanyName'];
+    $Email = $row['Email'];
+    $PhoneNum = $row['PhoneNum'];
+    $EmprID = $row['EmprID'];
+    $_SESSION['CompanyName'] = $CompanyName;
+    $_SESSION['Email'] = $Email;
+    $_SESSION['PhoneNum'] = $PhoneNum;
+    $_SESSION['EmprID'] = $EmprID;
     if(password_verify($password, $passEMPR)) {
         if ($countEMPR > 0){
-            $cookie_id = $row['EmprID'];
+            $cookie_id = $row['CompanyName'];
             setcookie($cookie_n, $cookie_id, time() + (3600), "/");
             echo 1;
         }
