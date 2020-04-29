488538e9b96336f4babc3ab39ad7bf54bd5d472c
almeida151 <60985086+almeida151@users.noreply.github.com>
2020-04-29 19:58:45 -0400

    Update EmployeeAccount.php

--- newfiles/EmployeeAccount.php
+++ newfiles/EmployeeAccount.php
@@ -4,6 +4,98 @@
 <head>
   <title>Employee Tab</title>
 
+<script>
+    resNum = 1;
+    //Add Resume to List
+    function addResume () {
+        var file = document.getElementById("myfile1");
+        var res1 = document.getElementById("res1").innerHTML;
+
+        document.getElementById("res1").innerHTML = res1 + "<h2 id=\"resume" + resNum + "2\">Resume " + resNum + "</h2>" +
+        "<p id=\"res" + resNum + "2Desc\">" + file + "</p>" +
+        "<button class=\"btn btn-success\" id=\"resume" + resNum + "2Del\" onclick=\"delResume(" + resNum + ")\">Delete</button>\t" +
+        "<button class=\"btn btn-success\" id=\"res" + resNum + "2View\">View</button>" +
+        "<hr id=\"hrRes" + resNum + "2\">";
+
+        resNum++;
+    }
+    //Delete resume from List
+    function delResume (rDelete) {
+        document.getElementById("resume" + rDelete + "2").remove();
+        document.getElementById("res" + rDelete + "2Desc").remove();
+        document.getElementById("resume" + rDelete + "2Del").remove();
+        document.getElementById("res" + rDelete + "2View").remove();
+        document.getElementById("hrRes" + rDelete + "2").remove();
+    }
+    //Open modal to send Message
+    function messag() {
+        var modal = document.getElementById("modalMessage");
+        var to = document.getElementById("to");
+        var topic = document.getElementById("topic");
+        var message = document.getElementById("message");
+        var btn = document.getElementById("composeBtn");
+        var btn2 = document.getElementById("sendBtn");
+        var span = document.getElementsByClassName("close");
+        var submit = "true";
+
+        btn.onclick = function() {
+            modal.style.display = "block";
+        }
+    
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
+    
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+        btn2.onclick = function() {
+            if(isNaN(to)) {} else {
+                submit = "false";
+                alert("Who not written");
+            }
+            if(isNaN(topic)) {} else {
+                submit = "false";
+                alert("Topic not written");
+            }
+            if(isNaN(message)) {} else {
+                submit = "false";
+                alert("Messsage not written");
+            }
+            if (submit == "true") {
+                modal.style.display = "none";
+            }
+            submit = "true";
+        }
+    }
+</script>
+
+<style>
+    /*Modal*/
+    .modal {
+        display: none;
+        position: fixed;
+        z-index: 1;
+        padding-top: 100px;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        overflow: auto;
+        background-color: RGB(0,0,0);
+        background-color: RGB(0,0,0,0.4);
+    }
+
+    .modal-content {
+        background-color: whitesmoke;
+        margin: auto;
+        padding: 14px 16px;
+        width: 80%;
+    }
+</style>
+
 <body style="background-color:lightgrey;"></body>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -20,7 +112,7 @@
   <nav class="nav navbar-inverse">
     <div class="container-fluid">
       <div class="navbar-header">
-        <a.logo class="navbar-top" href="#"><img src="CyberHirelogo.png" alt="Logo"
+        <a.logo class="navbar-top" href="#"><img src="CyberHire_Logo.png" alt="Logo"
             style="width:150px;height:39.8px;"></a>
       </div>
       <div class="float-right">
@@ -52,6 +144,7 @@
 
   <div class="tab-content">
     <div class="tab-pane fade" id="accHome" role="tabpanel">
+        <p><img src="Profile Image.jpg" alt="User Picture" width = "200" height = "200"></p>
       <?php
       session_start();
       $FName = $_SESSION['FName'];
@@ -82,14 +175,30 @@
     </div>
   -->
     <div class="tab-pane fade" id="msgs" role="tabpanel">
+        <p></p>
+        <button class = "btn btn-success" id ="composeBtn" onclick="messag()">Compose Message</button>
+        <div id="modalMessage" class="modal">
+            <div class="modal-content">
+                <p>To: <input type="text" id="to"><br></p>
+                <p>Topic: <input type="text" id="topic"><br></p>
+                <p>Message: <textarea id="message"></textarea></p>
+                <div><button class ="btn btn-success" id="sendBtn">Send</button>
+            </div></div>
+        </div>
+        <h2>Message 1</h2>
+        <p>Random nosadjsdjhasjdjkashdj asjdhkjashdlaskjhdjkasdkj askjdkjashkj jkdaskjdh shd nosadjsdjhasjdjkashdj asjdhkjashdlaskjhdjkasdkj askjdkjashkj jkdaskjdh shdnosadjsdjhasjdjkashdj asjdhkjashdlaskjhdjkasdkj askjdkjashkj jkdaskjdh shd</p>
+        <p><button class = "btn btn-success">Respond</button> <button class = "btn btn-success">Delete</button></p>
+        <hr>
+        <h2>Message 2</h2>
+        <p>sajdhajsdh asjdaskjdhjaskdkjas dkjasdkj hghjagds</p>
+        <p><button class = "btn btn-success">Respond</button> <button class = "btn btn-success">Delete</button></p>
     </div>
-
     <div class="tab-pane fade" id="List" role="tabpanel">
-      <h2>Resume 1</h2>
-      <p>Random nosadjsdjhasjdjkashdj asjdhkjashdlaskjhdjkasdkj askjdkjashkj jkdaskjdh shd </p>
-      <hr>
-      <h2>Resume 2</h2>
-      <p>sajdhajsdh asjdaskjdhjaskdkjas dkjasdkj hghjagds</p>
+        <form action="/action_page.php">
+            <label for="myfile1"></label>
+            <input type="file" id="myfile1"></form>
+            <button class="btn btn-success" onclick="addResume()">Submit</button>
+            <div id = "res1"></div>
     </div>
     <div class="tab-pane fade" id="acc" role="tabpanel">
       <center>
@@ -122,10 +231,6 @@
           <label for="pwd">Password:</label><br>
           <input type="pwd" class="form-control" id="pwd">
         </div><br>
-        <form action="upload.php" method="post" enctype="multipart/form-data">
-          <input type="file" name="myfile" id="myFile" class = "btn btn-success"  >
-          <input type="submit" value="Upload Resume" id= "myFile" name="submit" class = "btn btn-success" >
-        </form>
         <button class="btn btn-success" id = "myFile">Submit</button>
         </center>
       </form>
